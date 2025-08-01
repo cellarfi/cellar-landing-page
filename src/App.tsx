81a9d344db64c96d92518778a4dc6de500348547
Ahmad940 <Ahmadmuhammadmak5@gmail.com>
2025-08-01 15:39:48 +0100

    feat: added analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,39 @@
-import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import Landing from "@/pages/landing";
-import DownloadPage from "@/pages/download";
-import Privacy from "@/pages/privacy";
-import Terms from "@/pages/terms";
-import NotFound from "@/pages/not-found";
+import { Toaster } from '@/components/ui/toaster'
+import { TooltipProvider } from '@/components/ui/tooltip'
+import DownloadPage from '@/pages/download'
+import Landing from '@/pages/landing'
+import NotFound from '@/pages/not-found'
+import Privacy from '@/pages/privacy'
+import Terms from '@/pages/terms'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { Analytics } from '@vercel/analytics/react'
+import { Route, Switch } from 'wouter'
+import { queryClient } from './lib/queryClient'
 
 function Router() {
   return (
     <Switch>
-      <Route path="/" component={Landing} />
-      <Route path="/download" component={DownloadPage} />
-      <Route path="/privacy" component={Privacy} />
-      <Route path="/terms" component={Terms} />
+      <Route path='/' component={Landing} />
+      <Route path='/download' component={DownloadPage} />
+      <Route path='/privacy' component={Privacy} />
+      <Route path='/terms' component={Terms} />
       <Route component={NotFound} />
     </Switch>
-  );
+  )
 }
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <div className="dark">
+        <div className='dark'>
           <Toaster />
           <Router />
+          <Analytics />
         </div>
       </TooltipProvider>
     </QueryClientProvider>
-  );
+  )
 }
 
-export default App;
+export default App
